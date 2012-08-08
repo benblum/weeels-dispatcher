16409b1f475e42ef1e6013a584c9fc03d606051a
Ben Blum <benblum@gmail.com>
2012-08-08 15:46:21 -0400

    Fixed up security

--- src/main/java/org/weeels/dispatcher/controller/RideRequestController.java
+++ src/main/java/org/weeels/dispatcher/controller/RideRequestController.java
@@ -11,6 +11,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
